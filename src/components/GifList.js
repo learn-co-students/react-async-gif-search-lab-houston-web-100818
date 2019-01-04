a74f7da5bffc9578027e7174adf97c9a330807f7
barbaradecares <barbaradecares@hotmail.com>
2019-01-03 21:17:23 -0600

    done

---
+++ src/components/GifList.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+export default class GifList extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.props.gifs.map(gif => {
+            return (
+              <li>
+                <img src={gif.images.original.url} />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
